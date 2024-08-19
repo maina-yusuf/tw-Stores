d2835bbbe5e1815322617bc772b1e4f2f657e38e
unknown <yusufmustapha498@gmail.com>
2024-08-19 13:03:57 +0100

    Featured Page completed

--- js/script.js
+++ js/script.js
@@ -69,15 +69,46 @@ var Typed = new Typed(".main itnro",{
   
 });
 
-function increase(event) {
-  const button = event.target;
-  const cardBody = button.closest(".item1");
-  const quantityItem = cardBody.querySelector("input[type='number']");  let quantity = parseInt(quantityItem.value, 10);
-  quantity += 1;
-  quantityItem.value = quantity;
-}
+// function increase(event) {
+//   const button = event.target;
+//   const cardBody = button.closest(".item1");
+//   const quantityItem = cardBody.querySelector("input[type='number']");  let quantity = parseInt(quantityItem.value, 10);
+//   quantity += 1;
+//   quantityItem.value = quantity;
+// }
 
-const addbuttons = document.getElementsByClassName("fa fa-plus");
-for (let i = 0; i < addbuttons.length; i++) {
-  addbuttons[i].addEventListener("click", increase);
-}
+// const addbuttons = document.getElementsByClassName("fa fa-plus");
+// for (let i = 0; i < addbuttons.length; i++) {
+//   addbuttons[i].addEventListener("click", increase);
+// }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Get elements
+  const incrementButton = document.getElementById('increment');
+  const decrementButton = document.getElementById('decrement');
+  const quantityInput = document.querySelector('input[type="number"]');
+  const addToCartButton = document.querySelector('button');
+
+  // Event listener for increment button
+  incrementButton.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput.value);
+      if (currentValue < 100) {  // Max value is 100
+          quantityInput.value = currentValue + 1;
+      }
+  });
+
+  // Event listener for decrement button
+  decrementButton.addEventListener('click', () => {
+      let currentValue = parseInt(quantityInput.value);
+      if (currentValue > 1) {  // Min value is 1
+          quantityInput.value = currentValue - 1;
+      }
+  });
+
+  // Event listener for add to cart button
+  addToCartButton.addEventListener('click', () => {
+      let quantity = parseInt(quantityInput.value);
+      alert(`Added ${quantity} item(s) to the cart.`);
+      // Here you would normally send the data to the server or update the cart UI
+  });
+});
